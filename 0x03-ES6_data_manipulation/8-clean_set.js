190c20fc00a830769655db552fe4ab31a3eae28c
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-07-04 11:09:25 +0300

    Fixed code failure

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,12 @@
 function cleanSet(set, startString) {
-  if (startString === undefined || startString.length === 0) return '';
-  const resultArray = [];
-
-  for (const item of set) {
-    if (item.startsWith(startString)) {
-      resultArray.push(item.substring(startString.length));
-    }
+  if (startString === undefined || startString.length === 0) {
+    return '';
   }
+
+  const resultArray = Array.from(set)
+    .filter((item) => item.startsWith(startString))
+    .map((item) => item.slice(startString.length));
+
   return resultArray.join('-');
 }
 
