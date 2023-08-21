98ff8a714a77d4aeee56f299b0711256b4a0f091
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-08-21 18:53:24 +0300

    Fixed code failure

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,17 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-  const input = data.toString().trim();
-  process.stdout.write(`Your name is: ${input}\n`);
-});
-
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    const input = data.toString().trim();
+    process.stdout.write(`Your name is: ${input}\n`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString().trim()}\n`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
