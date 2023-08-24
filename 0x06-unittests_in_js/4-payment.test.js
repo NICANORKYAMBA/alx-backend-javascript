a8422781406ab9c501f065e6f2e935d36ff0e81b
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-08-24 20:54:58 +0300

    Fixed code failure

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,41 +1,33 @@
-// 4-payment.test.js
 const sinon = require('sinon');
-const assert = require('assert');
+const chai = require('chai');
+const expect = chai.expect;
 
-const Utils = require('./utils');
-const sendPaymentRequestToApi = require('./4-payment');
+const Utils = require('./utils.js');
+const sendPaymentRequestToApi = require('./4-payment.js');
 
 describe('sendPaymentRequestToApi', () => {
-  let calculateNumberStub;
-  let consoleLogSpy;
-
-  beforeEach(() => {
+  it('should call console.log with the right arguments', () => {
     // Create a stub for Utils.calculateNumber
-    calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
 
     // Create a spy to intercept console.log
-    consoleLogSpy = sinon.spy(console, 'log');
-  });
+    const consoleLogSpy = sinon.spy(console, 'log');
 
-  afterEach(() => {
-    // Restore the stub and spy to their original states after each test
-    calculateNumberStub.restore();
-    consoleLogSpy.restore();
-  });
-
-  it('should call Utils.calculateNumber with type SUM, a = 100, and b = 20', () => {
     // Call the function
     sendPaymentRequestToApi(100, 20);
 
-    // Assert that the stub was called with the expected arguments
-    sinon.assert.calledWithExactly(calculateNumberStub, 'SUM', 100, 20);
-  });
-
-  it('should log the correct message', () => {
-    // Call the function
-    sendPaymentRequestToApi(100, 20);
+    // Assert that Utils.calculateNumber was called with the correct arguments
+    expect(calculateNumberStub.calledWith('SUM', 100, 20)).to.be.true;
+    // Assert that Utils.calculateNumber was called only once
+    expect(calculateNumberStub.callCount).to.equal(1);
 
     // Assert that console.log was called with the correct message
-    sinon.assert.calledWithExactly(consoleLogSpy, 'The total is: 10');
+    expect(consoleLogSpy.calledWith('The total is: 10')).to.be.true;
+    // Assert that console.log was called only once
+    expect(consoleLogSpy.callCount).to.equal(1);
+
+    // Restore the stub and spy to their original states after the test
+    calculateNumberStub.restore();
+    consoleLogSpy.restore();
   });
 });
