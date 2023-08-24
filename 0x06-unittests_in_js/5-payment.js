23f362f01258be98a7a9e1b5a8f22d6400eeb10a
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-08-24 21:20:14 +0300

    Fixed code

--- 0x06-unittests_in_js/5-payment.js
+++ 0x06-unittests_in_js/5-payment.js
@@ -1,8 +1,8 @@
-const Utils = require('./utils.js');
+const Utils = require('./utils');
 
-function sendPaymentRequestToApi (totalAmount, totalShipping) {
-  const payment = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total is: ${payment}`);
-}
+const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
+  const totalCost = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${totalCost}`);
+};
 
 module.exports = sendPaymentRequestToApi;
