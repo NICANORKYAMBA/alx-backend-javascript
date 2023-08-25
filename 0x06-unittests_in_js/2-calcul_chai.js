5d6fbc174f93a63811c5aaffed55b394ba555610
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-08-25 12:33:20 +0300

    Fixed task 2 code failure

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,17 +1,17 @@
 function calculateNumber (type, a, b) {
-	const a_rounded = Math.round(a);
-	const b_rounded = Math.round(b);
+  const aRounded = Math.round(a);
+  const bRounded = Math.round(b);
 
   if (type === 'SUM') {
-    return a_rounded + b_rounded;
+    return aRounded + bRounded;
   } else if (type === 'SUBTRACT') {
-    return a_rounded - b_rounded;
+    return aRounded - bRounded;
   } else if (type === 'DIVIDE') {
-      return a_rounded / b_rounded;
-  } else if (b_rounded === 0) {
-	  return 'Error';
+    return aRounded / bRounded;
+  } else if (bRounded === 0) {
+    return 'Error';
   } else {
-	  return 'Error';
+    return 'Error';
   }
 }
 
